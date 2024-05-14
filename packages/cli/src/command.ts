08b1c8c66292d1b0a8d324a34eede36f2e319759
galileo-dev <fionn2200@gmail.com>
2024-05-14 12:03:14 +0100

    colors!

--- packages/cli/src/command.ts
+++ packages/cli/src/command.ts
@@ -1,16 +1,19 @@
+import chalk from "chalk";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { cowboyBoot } from "./cowboyBoot";
-
 import { signup_handler } from "./handler";
 
+const log = console.log;
+
 yargs(hideBin(process.argv))
-  .command("start", "Start the CLI", () => {
-    console.log(cowboyBoot);
-    console.log("Howdy Partner! Welcome to Yeehaw-Docs");
-    console.log(
-      "Commands: 'yeehaw register username password' -> signup for a new account"
-    );
+  .updateStrings({
+    "Options:": chalk.blue("Options:"),
+  })
+  .command("start", "Start the CLI", (yargs) => {
+    log(chalk.whiteBright(cowboyBoot));
+    log(chalk.green.bold("Howdy Partner! Welcome to Yeehaw-Docs"));
+    yargs.showHelp();
   })
   .command(
     "register <username> <password>",
