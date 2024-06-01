e7c844ed80ddd6e760ff3a17292dd8a6ef0dae42
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2024-06-01 11:55:19 +0100

    Update regex so password can be validated

--- packages/backend/models.ts
+++ packages/backend/models.ts
@@ -12,7 +12,7 @@ export const UsernameModel = t.String({
 export const PasswordModel = t.String({
   minLength: 8,
   default: "Password123!",
-  pattern: "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$",
+  pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}",
   description:
     "A password must have minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character. Please try again.",
 });
